abcdf12b1f08bbecd2a15d0337ae180cb5a89479
waqas-ahmd <65waqas@gmail.com>
2025-11-08 21:12:19 +0500

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import userRoutes from "./routes/user.js";
 import postRoutes from "./routes/post.js";
-import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
 
 dotenv.config();
@@ -9,11 +10,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
 app.get("/", (req, res) => {
   res.send("Blogs Application API");
 });
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
